56d68e9388f1d5442a7897a3c7ff106665b0bb06
EC2 Default User <ec2-user@ip-172-31-21-52.ec2.internal>
2022-01-05 05:31:44 +0000

    form_withに変更

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,19 @@ class Book < ApplicationRecord
   belongs_to :user
   attachment :profile_image
   has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
+  def commented_by?(user)
+    book_comments.where(user_id: user.id).exists?
+  end
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200}
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,11 @@ Rails.application.routes.draw do
   resources :books
 
   resources :books, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+
     resources :book_comments, only: [:create, :destroy]
   end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
